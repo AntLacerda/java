9410c5ec30fc49265e60e886b1dacd488f4b1a0f
AntLacerda <lacerdaneto99@hotmail.com>
2025-12-23 22:51:56 -0300

    Feat: Exercicio de exceções

--- src/exercicios_sec14/application/Program.java
+++ src/exercicios_sec14/application/Program.java
@@ -1,4 +1,43 @@
 package exercicios_sec14.application;
 
+
+import exercicios_sec14.model.entities.Account;
+import exercicios_sec14.model.exceptions.DomainException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Holder: ");
+            String holder = sc.nextLine();
+            System.out.print("Initial balance: ");
+            Double balance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            Double withdrawLimit = sc.nextDouble();
+
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            System.out.println();
+            System.out.print("Enter amount for withdraw: ");
+            double amount = sc.nextDouble();
+
+            account.withdraw(amount);
+
+            System.out.println(account.toString());
+
+        } catch (DomainException e) {
+            System.out.println("Withdraw error: " + e.getMessage());
+        }
+
+        sc.close();
+    }
 }
